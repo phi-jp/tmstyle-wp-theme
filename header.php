2ef2eea6d123241dadc1dfcf810f682655cb40f5
phi <phi1618jp@gmail.com>
2012-07-26 19:09:07 +0900

    add social link to header.

--- header.php
+++ header.php
@@ -4,6 +4,15 @@
         <h2><?php bloginfo('description'); ?></h2>
     </hgroup>
     
+    <div class="links">
+        <ul>
+            <li><a href="https://twitter.com/phi_jp" class="twitter" alt="Twitter"></a></li>
+            <li><a href="https://plus.google.com/113051166787856683749" class="gplus" alt="Goolge Plus"></a></li>
+            <li><a href="https://github.com/phi1618" class="github" alt="GitHub"></a></li>
+            <li><a href="http://tmlife.net/feed" class="rss" alt="RSS"></a></li>
+        </ul>
+    </div>
+    
     <nav id="nav">
         <?php
             wp_nav_menu(array(
